4e090ec86637fb8e5e7d03c3892cb7b24672cd92
Prabhu <prabhuprathamesh444@gmail.com>
2024-10-21 08:17:25 +0530

    login design

--- src/components/Auth/Loigin.jsx
+++ src/components/Auth/Loigin.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
+import myImage from './login11.png';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -23,32 +24,40 @@ const Login = () => {
     };
 
     return (
-        <div className='flex items-center justify-center h-screen w-screen'>
-            <div className='border-2 p-20 border-emerald-600 rounded-xl'>
-                <form onSubmit={submithandler} className="flex flex-col items-center justify-center">
-                    <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        className='outline-none bg-transparent border-2 border-emerald-600 font-medium py-2 px-6 text-lg rounded-full placeholder:text-gray-400'
-                        type="email"
-                        placeholder='Enter Your Email'
-                    />
-                    <input
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        className='outline-none bg-transparent border-2 border-emerald-600 font-medium py-2 px-6 text-lg rounded-full placeholder:text-gray-400 mt-3'
-                        type="password"
-                        placeholder='Enter Your Password'
-                    />
-                    <button className='text-white outline-none font-semibold border-none hover:bg-emerald-700 bg-emerald-600 py-2 px-8 text-lg rounded-full w-full mt-7'>
-                        Log in
-                    </button>
-                </form>
+        <div className='flex items-center justify-center h-screen w-screen border-white-500 bg-[#86bfba]'>
+          <div className='flex w-[60%] h-[60%] rounded-3xl overflow-hidden'>
+            <div className='w-[50%] '>
+              <img src={myImage} className='object-cover h-full w-full' alt='' />
             </div>
+            <div className=' p-20  w-[50%] flex items-center justify-center bg-[#1c4f4a] h-full'>
+            <form 
+            onSubmit={(e)=>{
+              submithandler(e);
+            }}
+            className="flex flex-col items-center justify-center">
+              <h1 className='font-bold text-[35px] mb-10 font-arial'>Login </h1>
+              <input 
+              value={email}
+              onChange={(e)=>{
+                setEmail(e.target.value)
+              }}
+              required 
+              className='outline-none bg-transparent border-2 font-medium py-2 px-6 text-lg rounded-full placeholder:text-gray-400' type="email" placeholder='Enter Your Email' 
+              />
+              <input 
+              value={password}
+              onChange={(e)=>{
+                setPassword(e.target.value)
+              }}
+              required 
+              className='outline-none bg-transparent border-2 font-medium py-2 px-6 text-lg rounded-full placeholder:text-gray-400 mt-3' type="password" placeholder='Enter Your Password' />
+              <button className='text-white outline-none font-semibold border-none hover:bg-emerald-700 bg-emerald-600 py-2 px-8 text-lg rounded-full w-[270px] mt-7'>Log in</button>
+            </form>
+            </div>
+          </div>
         </div>
-    );
-};
-
-export default Login;
+      )
+    }
+    
+    export default Login
+    
